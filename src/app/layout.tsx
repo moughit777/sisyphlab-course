d43a98c4571ad70c5a76ce7420b61c6d81f527e2
moughit777 <moughitmoughit777@gmail.com>
2026-09-09 21:12:17 +0100

    style: apply IBM Plex Sans Arabic as primary font in body className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <meta name="theme-color" content="#070B1A" />
       </head>
-      <body className={`${tajawal.className} ${ibmPlex.variable} antialiased`}>
+      <body className={`${ibmPlex.className} antialiased`}>
         <GlobalBackground />
         {children}
       </body>
